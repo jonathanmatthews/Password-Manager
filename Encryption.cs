40c05a2b3628af35eee43f91e07994782d27a0b6
Jonathan <44363860+jonathanmatthews@users.noreply.github.com>
2019-09-06 19:42:42 +0100

    Added RSA encryption for strings

--- Encryption.cs
+++ Encryption.cs
@@ -2,22 +2,50 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 
-static class Encryption {
-    public static string[] KeyGen () {
-        // Generate a pair of RSA keys in XML format.
-        string priv;
-        string pub;
+namespace PasswordManager {
+    static class Encryption {
         
-        //var parameters = new CspParameters(1); // RSA Full.
-        //parameters.Flags = CspProviderFlags.UseMachineKeyStore;
+        public static string[] KeyGen () {
+            // Generate a pair of RSA keys in XML format.
+            string priv;
+            string pub;
+            
+            //var parameters = new CspParameters(1); // RSA Full.
+            //parameters.Flags = CspProviderFlags.UseMachineKeyStore;
+            
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider()) {
+                priv = rsa.ToXmlString(true);
+                pub = rsa.ToXmlString(false);
+            } // Using RSA
+            
+            return new string[] {pub, priv};
+        } // KeyGen
         
-        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider()) {
-            priv = rsa.ToXmlString(true);
-            pub = rsa.ToXmlString(false);
-        } // Using RSA
+        public static byte[] Encrypt(string pubKey, string data) {
+            // Encrypt a string in XML format using XML format pubKey.
+            byte[] encryptedDataBytes;
+            
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider()) {
+                rsa.FromXmlString(pubKey);
+                byte[] dataBytes = ASCIIEncoding.ASCII.GetBytes(data);
+                encryptedDataBytes = rsa.Encrypt(dataBytes, true); // Not sure about padding bool.
+            }
+            
+            return encryptedDataBytes;
+        } // Encrypt
         
-        return new string[] {pub, priv};
-    } // KeyGen
-    
-
-} // Encryption
+        public static string Decrypt (string privKey, byte[] encryptedDataBytes) {
+            // Decrypt byte array of encrypted string using XML format privKey.
+            string data;
+            
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider()) {
+                rsa.FromXmlString(privKey);
+                byte[] dataBytes = rsa.Decrypt(encryptedDataBytes, true);
+                data = ASCIIEncoding.ASCII.GetString(dataBytes);
+            }
+            
+            return data;
+        } // Decrypt
+        
+    } // Encryption
+} // PasswordManager
--- Main.cs
+++ Main.cs
@@ -1,12 +1,26 @@
 using System;
+using PasswordManager;
+using System.Text;
 
 class Program {
     static void Main () {
         string[] keys;
-        keys = Encryption.KeyGen();
+        keys = Encryption.KeyGen(); // {pub, priv}
         
+        string testWord = "cactus";
+        
+        byte[] encrypted = Encryption.Encrypt(keys[0], testWord);
+        string decrypted = Encryption.Decrypt(keys[1], encrypted);
+        
+        
+        /*
         Console.WriteLine(keys[0]);
         Console.Write("\n\n");
         Console.WriteLine(keys[1]);
+        */
+        
+        Console.WriteLine("unencrypted: {0}", testWord);
+        Console.WriteLine("encrypted: {0}", ASCIIEncoding.ASCII.GetString(encrypted));
+        Console.WriteLine("decrypted: {0}", decrypted);
     }
 }
